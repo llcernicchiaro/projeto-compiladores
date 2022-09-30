b306bcc94e7a6519f334a5c74e127ae06d5bc160
Lorenzo Lopes Cernicchiaro <lopescernicchiaro@gmail.com>
2022-09-30 17:54:14 -0300

    entrega etapa6

--- etapa6/asm.c
+++ etapa6/asm.c
@@ -1,3 +1,6 @@
+// Douglas Gehring - 00243682
+// Lorenzo Cernicchiaro - 00287718
+
 #include "asm.h"
 
 // 	.section	__DATA,__data
@@ -7,8 +10,9 @@
 // L_.str:                                 ## @.str
 // 	.asciz	"%d"
 
-void generateASM(TAC *tac, FILE* fout) {
-    
+void generateASM(TAC *tac, FILE *fout)
+{
+
     // Sempre no inicio estrutura padrao:
 
     printASMInitStatement(fout);
@@ -17,138 +21,137 @@ void generateASM(TAC *tac, FILE* fout) {
 
     bool isCurrentFunctionMain = false;
 
-    for(;tac; tac=tac->next) 
+    for (; tac; tac = tac->next)
     {
-        switch(tac->type)
+        switch (tac->type)
         {
-            case TAC_SYMBOL:
+        case TAC_SYMBOL:
             // Fazer nada
             break;
-            case TAC_ADD:
-                printASMElement(fout, "TAC_ADD");
+        case TAC_ADD:
+            printASMElement(fout, "TAC_ADD");
             break;
-            case TAC_SUB:
-                printASMElement(fout, "TAC_SUB");
+        case TAC_SUB:
+            printASMElement(fout, "TAC_SUB");
             break;
-            case TAC_MULT:
-                printASMElement(fout, "TAC_MULT");
+        case TAC_MULT:
+            printASMElement(fout, "TAC_MULT");
             break;
-            case TAC_DIV:
-                printASMElement(fout, "TAC_DIV");
+        case TAC_DIV:
+            printASMElement(fout, "TAC_DIV");
             break;
-            case TAC_COPY:
-                printASMElement(fout, "TAC_COPY");
+        case TAC_COPY:
+            printASMElement(fout, "TAC_COPY");
             break;
-            case TAC_LABEL:
-                printASMElement(fout, "TAC_LABEL");
+        case TAC_LABEL:
+            printASMElement(fout, "TAC_LABEL");
             break;
-            case TAC_BEGIN_FUN:
-                isCurrentFunctionMain = isMainFunction(tac->res->text);
-                printASMBeginOfFunction(fout, tac->res->text);
+        case TAC_BEGIN_FUN:
+            isCurrentFunctionMain = isMainFunction(tac->res->text);
+            printASMBeginOfFunction(fout, tac->res->text);
             break;
-            case TAC_END_FUN:
-                printASMEndOfFunction(fout, isCurrentFunctionMain);
-                isCurrentFunctionMain = false;
+        case TAC_END_FUN:
+            printASMEndOfFunction(fout, isCurrentFunctionMain);
+            isCurrentFunctionMain = false;
             break;
-            case TAC_IFZ:
-                printASMElement(fout, "TAC_IFZ");
+        case TAC_IFZ:
+            printASMElement(fout, "TAC_IFZ");
             break;
-            case TAC_JUMP:
-                printASMElement(fout, "TAC_JUMP");
+        case TAC_JUMP:
+            printASMElement(fout, "TAC_JUMP");
             break;
-            case TAC_CALL:
-                printASMElement(fout, "TAC_CALL");
+        case TAC_CALL:
+            printASMElement(fout, "TAC_CALL");
             break;
-            case TAC_ARG:
-                printASMElement(fout, "TAC_ARG");
+        case TAC_ARG:
+            printASMElement(fout, "TAC_ARG");
             break;
-            case TAC_RETURN:
-                printASMElement(fout, "TAC_RETURN");
+        case TAC_RETURN:
+            printASMElement(fout, "TAC_RETURN");
             break;
-            case TAC_PRINT:
-                printASMElement(fout, "TAC_PRINT");
+        case TAC_PRINT:
+            printASMElement(fout, "TAC_PRINT");
             break;
-            case TAC_READ:
-                printASMElement(fout, "TAC_READ");
+        case TAC_READ:
+            printASMElement(fout, "TAC_READ");
             break;
-            case TAC_LESS:
-                printASMElement(fout, "TAC_LESS");
+        case TAC_LESS:
+            printASMElement(fout, "TAC_LESS");
             break;
-            case TAC_GREATER:
-                printASMElement(fout, "TAC_GREATER");
+        case TAC_GREATER:
+            printASMElement(fout, "TAC_GREATER");
             break;
-            case TAC_OR:
-                printASMElement(fout, "TAC_OR");
+        case TAC_OR:
+            printASMElement(fout, "TAC_OR");
             break;
-            case TAC_AND:
-                printASMElement(fout, "TAC_AND");
+        case TAC_AND:
+            printASMElement(fout, "TAC_AND");
             break;
-            case TAC_LE:
-                printASMElement(fout, "TAC_LE");
+        case TAC_LE:
+            printASMElement(fout, "TAC_LE");
             break;
-            case TAC_GE:
-                printASMElement(fout, "TAC_GE");
+        case TAC_GE:
+            printASMElement(fout, "TAC_GE");
             break;
-            case TAC_EQ:
-                printASMElement(fout, "TAC_EQ");
+        case TAC_EQ:
+            printASMElement(fout, "TAC_EQ");
             break;
-            case TAC_DIF:
-                printASMElement(fout, "TAC_DIF");
+        case TAC_DIF:
+            printASMElement(fout, "TAC_DIF");
             break;
-            case TAC_FUNC_CALL:
-                printASMElement(fout, "TAC_FUNC_CALL");
+        case TAC_FUNC_CALL:
+            printASMElement(fout, "TAC_FUNC_CALL");
             break;
-            case TAC_JFALSE:
-                printASMElement(fout, "TAC_JFALSE");
+        case TAC_JFALSE:
+            printASMElement(fout, "TAC_JFALSE");
             break;
-            case TAC_IF_ELSE:
-                printASMElement(fout, "TAC_IF_ELSE");
+        case TAC_IF_ELSE:
+            printASMElement(fout, "TAC_IF_ELSE");
             break;
-            case TAC_VEC_DECLARATION:
-                printASMElement(fout, "TAC_VEC_DECLARATION");
+        case TAC_VEC_DECLARATION:
+            printASMElement(fout, "TAC_VEC_DECLARATION");
             break;
-            case TAC_VAR_DECLARATION:
-                printASMElement(fout, "TAC_VAR_DECLARATION");
+        case TAC_VAR_DECLARATION:
+            printASMElement(fout, "TAC_VAR_DECLARATION");
             break;
-            case TAC_PARAM:
-                printASMElement(fout, "TAC_PARAM");
+        case TAC_PARAM:
+            printASMElement(fout, "TAC_PARAM");
             break;
-            default:
-                printf("TAC TYPE: %d", tac->type);
-                break;
+        default:
+            printf("TAC TYPE: %d", tac->type);
+            break;
         }
     }
 
     hashPrintASM(fout);
 
     printASMEndStatement(fout);
-    
 }
 
-bool isMainFunction(char *identifier) 
+bool isMainFunction(char *identifier)
 {
     return strcmp(identifier, "main") == 0;
 }
 
-void printASMElement(FILE* fout, char* element) 
+void printASMElement(FILE *fout, char *element)
 {
-    //fprintf(fout, "##%s\n", element);
+    // fprintf(fout, "##%s\n", element);
 }
 
-void printASMEndStatement(FILE *fout) 
+void printASMEndStatement(FILE *fout)
 {
     fprintf(fout, ".subsections_via_symbols\n");
 }
 
-void printASMInitStatement(FILE* fout) 
+void printASMInitStatement(FILE *fout)
 {
     fprintf(fout, "\t.section	__TEXT,__text,regular,pure_instructions\n");
 }
 
-void printASMBeginOfFunction(FILE* fout, char* identifier) 
+void printASMBeginOfFunction(FILE *fout, char *identifier)
 {
     fprintf(
-        fout, 
+        fout,
         "\t.globl	_%s                           ## -- Begin function %s\n"
         "_%s:                                  ## @%s\n"
         "\t.cfi_startproc\n"
@@ -156,17 +159,16 @@ void printASMBeginOfFunction(FILE* fout, char* identifier)
         "\tpushq	%%rbp\n"
         "\tmovq	%%rsp, %%rbp\n"
         "\t.cfi_def_cfa_register %%rbp\n",
-        identifier, identifier, identifier, identifier
-    );
+        identifier, identifier, identifier, identifier);
 }
 
-void printASMEndOfFunction(FILE* fout, bool isMain) 
+void printASMEndOfFunction(FILE *fout, bool isMain)
 {
     if (isMain)
     {
         fprintf(fout, "\txorl	%%eax, %%eax\n\tpopq	%%rbp\n\tretq\n\t.cfi_endproc\n                           ## -- End of function\n");
-    } 
-    else 
+    }
+    else
     {
         fprintf(fout, "\tpopq	%%rbp\n\tretq\n\t.cfi_endproc\n                           ## -- End of function\n");
     }
--- etapa6/asm.h
+++ etapa6/asm.h
@@ -1,11 +1,14 @@
- #include <stdio.h>
- #include "tac.h"
- #include <stdbool.h>
+// Douglas Gehring - 00243682
+// Lorenzo Cernicchiaro - 00287718
 
-void generateASM(TAC *tac, FILE* fout);
-void printASMInitStatement(FILE* fout);
-void printASMElement(FILE* fout, char* element);
-void printASMBeginOfFunction(FILE* fout, char* identifier);
-void printASMEndOfFunction(FILE* fout, bool isMain);
+#include <stdio.h>
+#include "tac.h"
+#include <stdbool.h>
+
+void generateASM(TAC *tac, FILE *fout);
+void printASMInitStatement(FILE *fout);
+void printASMElement(FILE *fout, char *element);
+void printASMBeginOfFunction(FILE *fout, char *identifier);
+void printASMEndOfFunction(FILE *fout, bool isMain);
 bool isMainFunction(char *identifier);
 void printASMEndStatement(FILE *fout);
